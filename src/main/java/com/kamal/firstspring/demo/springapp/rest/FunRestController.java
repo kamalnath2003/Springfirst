dac6b534d822873784a1b29128e0770e793e4d56
kamalnah2003 <54796340+kamalnah2003@users.noreply.github.com>
2024-03-07 15:15:07 +0530

    hi

--- src/main/java/com/kamal/firstspring/demo/springapp/rest/FunRestController.java
+++ src/main/java/com/kamal/firstspring/demo/springapp/rest/FunRestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class FunRestController {
     @GetMapping("/")
     public  String Sayhello(){
-        return "Hello world";
+        return "Hello Kamal";
     }
 
 }
