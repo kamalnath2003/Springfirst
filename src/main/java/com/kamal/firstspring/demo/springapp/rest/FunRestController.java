413b8357eba893b0a12e7ec02d1a309c93368617
kamalnah2003 <54796340+kamalnah2003@users.noreply.github.com>
2024-03-08 22:48:19 +0530

    m

--- src/main/java/com/kamal/firstspring/demo/springapp/rest/FunRestController.java
+++ src/main/java/com/kamal/firstspring/demo/springapp/rest/FunRestController.java
@@ -1,13 +1,23 @@
 package com.kamal.firstspring.demo.springapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
 @RestController
 public class FunRestController {
+    @Value("${coach.name}")
+    private String name;
     @GetMapping("/")
+
     public  String Sayhello(){
-        return "Hello Kamal";
+        return name;
+    }
+    @GetMapping("/workout")
+    public String Sayworkout(){
+        return  "workout";
     }
 
 }
